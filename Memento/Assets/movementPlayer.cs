5f0ed99783136e0b70160028393ad174929944e8
Poplar <laudzansyawaliandy98678@gmail.com>
2024-11-24 01:47:06 +0700

    collider + tutorial map(1/3)

--- Memento/Assets/movementPlayer.cs
+++ Memento/Assets/movementPlayer.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MovementPlayer : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Transform movePoint;
     public Animator animator;
+    public Tilemap obstacle;
+    public LayerMask unmove;
+
+    
+
 
     void Start()
     {
@@ -18,22 +24,30 @@ public class MovementPlayer : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
         
         
+        Vector3Int obstacleMap = obstacle.WorldToCell(movePoint.position - new Vector3(0, 0.5f, 0) + new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f));
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f && obstacle.GetTile(obstacleMap) == null )
         {
            
-            if (Input.GetAxisRaw("Horizontal") != 0f)
+            if (Input.GetAxisRaw("Horizontal") != 0f && obstacle.GetTile(obstacleMap) == null)
             {
-                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                animator.SetFloat("horizontal",Input.GetAxisRaw("Horizontal"));
-                animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, unmove)){
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    animator.SetFloat("horizontal",Input.GetAxisRaw("Horizontal"));
+                    animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+                }
+                
             }
 
-            if (Input.GetAxisRaw("Vertical") != 0f)
-            {
-                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                animator.SetFloat("horizontal",Input.GetAxisRaw("Horizontal"));
-                animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+            if (Input.GetAxisRaw("Vertical") != 0f && obstacle.GetTile(obstacleMap) == null)
+            {   
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, unmove)){
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    animator.SetFloat("horizontal",Input.GetAxisRaw("Horizontal"));
+                    animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+                }
+                
             }
             
             
@@ -41,5 +55,13 @@ public class MovementPlayer : MonoBehaviour
 
         animator.SetFloat("speed", Mathf.Sqrt((Input.GetAxisRaw("Horizontal")*Input.GetAxisRaw("Horizontal")) + (Input.GetAxisRaw("Vertical")*Input.GetAxisRaw("Vertical"))));
 
+    }
+
+    void FixedUpdate(){
+    }
+
+    void OnDrawGizmos(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(movePoint.position - new Vector3(0, 0.5f, 0) + new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f),0.2f);
     }
 }
